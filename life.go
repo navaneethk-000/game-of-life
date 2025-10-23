3b294decc97d70f7e4919042d304a9a0ea706b71
Navaneeth <navaneethk969@gmail.com>
2025-10-23 14:47:41 +0530

    Implements test case to display grid

--- life.go
+++ life.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 type Grid struct {
@@ -9,26 +10,61 @@ type Grid struct {
 	data [][]bool
 }
 
-func NewGrid(size uint) Grid {
-	grid := Grid{
+func NewGrid(size uint, live ...int) Grid {
+	grid := make([][]bool, size)
+
+	for i := 0; i < int(size); i++ {
+		grid[i] = make([]bool, size)
+	}
+	for i := 0; i < len(live); i = i + 2 {
+		x := live[i]
+		y := live[i+1]
+		grid[x][y] = true
+	}
+
+	return Grid{
 		size: size,
-		data: make([][]bool, size),
+		data: grid,
 	}
-
-	for i := range grid.data {
-		grid.data[i] = make([]bool, size)
-	}
-	return grid
-}
-
-func Display(grid Grid) string {
-
-	return ""
 
 }
 
-func CountAliveNegighbours(grid Grid, x uint, y uint) uint {
-	return 0
+func DisplayGrid(grid Grid) string {
+	var builder strings.Builder
+	for _, row := range grid.data {
+		for _, i := range row {
+			if i {
+				builder.WriteString("* ")
+			} else {
+				builder.WriteString("x ")
+			}
+		}
+		builder.WriteString("\n")
+	}
+	return builder.String()
+
+}
+
+// func CountNeighbours(grid Grid, x uint, y uint) uint {
+
+// 	return 0
+// }
+
+func CountAliveNeighbours(grid Grid, x uint, y uint) uint {
+
+	// size := int(grid.size)
+	var count uint
+	for row := int(x) - 1; row <= int(x)+1; row++ {
+
+		for col := int(y) - 1; col <= int(y)+1; col++ {
+			if row == int(x) && col == int(y) {
+				continue
+			}
+			count++
+
+		}
+	}
+	return count
 }
 
 func Rungeneration() {
@@ -37,8 +73,7 @@ func Rungeneration() {
 
 func main() {
 
-	newGrid := NewGrid(uint(3))
-	fmt.Println(newGrid)
-	Display(newGrid)
-	fmt.Println(Display(newGrid))
+	newGrid := NewGrid(6)
+	fmt.Println(DisplayGrid(newGrid))
+	fmt.Println(CountAliveNeighbours(newGrid, 1, 1))
 }
