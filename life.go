491a752bd70a7c0ee24a2b6b1c1402ac638b0690
Navaneeth <navaneethk969@gmail.com>
2025-10-23 20:02:54 +0530

    Implements conditions to count neighbour cells at top edge

--- life.go
+++ life.go
@@ -78,6 +78,11 @@ func CountAliveNeighbours(grid Grid, x uint, y uint) uint {
 		return b2i(grid.data[x-1][y]) + b2i(grid.data[x-1][y-1]) + b2i(grid.data[x][y-1])
 	}
 
+	//top edge
+	if x == 0 {
+		return b2i(grid.data[x][y-1]) + b2i(grid.data[x][y+1]) + b2i(grid.data[x+1][y-1]) + b2i(grid.data[x+1][y]) + b2i(grid.data[x+1][y+1])
+	}
+
 	return 0
 }
 
